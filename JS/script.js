59dee26de6d1884fb5d64d39874498c05c767139
Frozen <frozendron1@gmail.com>
2022-04-16 17:50:41 +0300

    add script

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,9 @@
 $(".img-gall").click(function(){
   $("#"+this.id).toggleClass("opas1", 600, "easeOutSine");
 })
+
+$(".sup").click(function(){
+  $(".num").hide();
+  $(".num").text('4149 6293 8660 3419');
+  $(".num").show('slow');
+})
